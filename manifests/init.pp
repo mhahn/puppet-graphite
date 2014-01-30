8ce2a6b0350475d5e83afcb4f0847d8cd8c5101f
Michael Hahn <mhahn@eventbrite.com>
2014-01-29 16:47:34 -0800

    Anchor the graphite module

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,10 +11,22 @@
 # * Implement user creation.
 #
 class graphite {
+
+  anchor { 'graphite::begin': }
+  anchor { 'graphite::end': }
+
   include graphite::params
   include graphite::carbon
   include graphite::whisper
   include graphite::web
   include graphite::config
+
+  Anchor['graphite::begin'] ->
+  Class['graphite::params'] ->
+  Class['graphite::carbon'] ->
+  Class['graphite::web'] ->
+  Class['graphite::config'] ->
+  Anchor['graphite::end']
+
 }
 
