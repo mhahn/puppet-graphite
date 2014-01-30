4503b0838b2ef54cb944c89ce8f8b70b0154676c
Michael Hahn <mhahn@eventbrite.com>
2014-01-30 15:19:26 -0800

    Wow, puppet, you're really bothering me

--- manifests/web/package.pp
+++ manifests/web/package.pp
@@ -2,16 +2,17 @@
 #
 class graphite::web::package inherits graphite::web::params {
 
-  package { 'django':
-    ensure   => '1.5.2',
-    provider => 'pip',
-    require  => Package['python-pip'],
+  exec { 'install-django':
+    logoutput => true,
+    command   => '/usr/local/bin/pip install -U django==1.5.2',
+    unless    => '/usr/local/bin/pip freeze | /bin/grep -i django==1.5.2',
+    require   => Package['python-pip'],
   }
 
   package { $package_name:
     ensure   => latest,
     provider => 'pip',
-    require  => Package['django'],
+    require  => Exec['install-django'],
   }
 
 }
