92fbba537430aa0c04990fe6d36a61bb0f7216b3
Michael Hahn <mhahn@eventbrite.com>
2014-01-30 13:10:38 -0800

    Anchor carbon and whipser classes

--- manifests/carbon.pp
+++ manifests/carbon.pp
@@ -1,9 +1,21 @@
 # = Class: graphite::carbon
 #
 class graphite::carbon {
+
+  anchor { 'graphite::carbon::begin': }
+  anchor { 'graphite::carbon::end': }
+
   include graphite::carbon::params
   include graphite::carbon::package
   include graphite::carbon::config
   include graphite::carbon::service
+
+  Anchor['graphite::carbon::begin'] ->
+    Class['graphite::carbon::params'] ->
+    Class['graphite::carbon::package'] ->
+    Class['graphite::carbon::config'] ->
+    Class['graphite::carbon::service'] ->
+    Anchor['graphite::carbon::end']
+
 }
 
--- manifests/whisper.pp
+++ manifests/whisper.pp
@@ -12,8 +12,16 @@
 #
 class graphite::whisper {
 
+  anchor { 'graphite::whisper::begin': }
+  anchor { 'graphite::whisper::end': }
+
   include graphite::whisper::package
   include graphite::whisper::config
+
+  Anchor['graphite::whisper::begin'] ->
+    Class['graphite::whisper::package'] ->
+    Class['graphite::whisper::config'] ->
+    Anchor['graphite::whisper::end']
 
 }
 
