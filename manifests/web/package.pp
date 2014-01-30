69c7cc9cfe7584934c32d0227629f7941ee7e999
Michael Hahn <mhahn@eventbrite.com>
2014-01-30 14:47:01 -0800

    Specify the package name

--- manifests/web/package.pp
+++ manifests/web/package.pp
@@ -1,8 +1,8 @@
 # Class: graphite::web::package
 #
-class graphite::web::package {
+class graphite::web::package inherits graphite::web::params {
 
-  package { ['graphite-web', 'django-tagging']:
+  package { $package_name:
     ensure   => present,
     provider => 'pip',
     require  => Package['python-pip'],
--- manifests/web/params.pp
+++ manifests/web/params.pp
@@ -4,5 +4,6 @@ class graphite::web::params {
 
   $config_dir = '/opt/graphite/webapp/graphite'
   $service_name = 'apache2'
+  $package_name = ['graphite-web', 'django-tagging']
 
 }
