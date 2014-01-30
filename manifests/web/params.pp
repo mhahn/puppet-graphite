8cd652a616b1c9cb6c8cf0bde15784977e46b737
Michael Hahn <mhahn@eventbrite.com>
2014-01-30 14:56:23 -0800

    Inherit graphite params

--- manifests/web/params.pp
+++ manifests/web/params.pp
@@ -1,6 +1,6 @@
 # Class: graphite::web::params
 #
-class graphite::web::params {
+class graphite::web::params inherits graphite::params {
 
   $config_dir = '/opt/graphite/webapp/graphite'
   $service_name = 'apache2'
