daf198901d48fcf8714096abd3abd71b14380f24
Michael Hahn <mhahn@eventbrite.com>
2014-01-30 15:11:50 -0800

    Require version 1.5.2 of vagrant for root user

--- manifests/web/package.pp
+++ manifests/web/package.pp
@@ -2,11 +2,16 @@
 #
 class graphite::web::package inherits graphite::web::params {
 
-  package { $package_name:
-    ensure   => latest,
+  package { 'django':
+    ensure   => '1.5.2',
     provider => 'pip',
     require  => Package['python-pip'],
   }
 
-}
+  package { $package_name:
+    ensure   => latest,
+    provider => 'pip',
+    require  => Package['django'],
+  }
 
+}
--- manifests/web/params.pp
+++ manifests/web/params.pp
@@ -4,6 +4,6 @@ class graphite::web::params inherits graphite::params {
 
   $config_dir = '/opt/graphite/webapp/graphite'
   $service_name = 'apache2'
-  $package_name = ['graphite-web', 'django-tagging', 'django']
+  $package_name = ['graphite-web', 'django-tagging']
 
 }
