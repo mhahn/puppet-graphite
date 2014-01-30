0d309c33ee82486ea855e90da82b3d9b0cd7b068
Michael Hahn <mhahn@eventbrite.com>
2014-01-29 16:58:25 -0800

    Properly anchor the web nested classes

--- manifests/web/service.pp
+++ manifests/web/service.pp
@@ -1,6 +1,8 @@
 # Class: graphite::web::service
 #
 class graphite::web::service {
+  include $graphite::web::params
+
   $manage_httpd = $graphite::web::params::manage_httpd
   $service_name = $graphite::web::params::service_name
 
